cffa0817b38ca57a654904b825f6cd5dde9763c1
Sumanthkanuri10 <sumanthkanuri10@gmail.com>
2024-03-25 12:04:36 -0600

    first Version

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject.jsx";
+import ProjectSidebar from "./components/ProjectSidebar.jsx";
+import NoProjectSelected from "./components/noProjectSelected.jsx";
+
+function App() {
+  const [projectState,setProjectState]=useState({
+    selectedProjectId:undefined,
+    projects:[]
+}
+);
+function handleStartAddProject(){
+    setProjectState(prevState=>{
+        return{
+            ...prevState
+            ,selectedProjectId:null,
+        };
+    });
+}
+function handleAddProject(projectData){
+setProjectState(prevState=>{
+  const newProject={
+    ...projectData,
+    id:Math.random
+  };
+  return{
+    ...prevState, projects:[...prevState.projects, newProject]
+
+  };
+});
+}
+console.log(projectState)
+let content;
+if (projectState.selectedProjectId===null){
+  content=<NewProject onAdd={handleAddProject}/>
+} else if (projectState.selectedProjectId===undefined){
+  content=<NoProjectSelected onStartAddProject={handleStartAddProject}/>
+}
+  return (
+    <main className="h-screen my-8 flex gap-8 ">
+      <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
+     </main>)
+}
+
+export default App;
